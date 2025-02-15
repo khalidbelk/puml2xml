93a8bd903c4e250f315066f610769dcfabcb6b8f
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2025-02-15 05:34:29 +0100

    feat: add puml2xml to the header documentation tags

--- src/xml_chunks.ml
+++ src/xml_chunks.ml
@@ -11,6 +11,7 @@ let xml_header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><XMI xmlns:UML=\"hre
         <XMI.documentation>
             <XMI.exporter>PlantUML</XMI.exporter>
             <XMI.exporterVersion>1.2024.7</XMI.exporterVersion>
+            <XMI.creator>puml2xml</XMI.creator>
         </XMI.documentation>
         <XMI.metamodel xmi.name=\"UML\" xmi.version=\"1.4\"/>
     </XMI.header>
