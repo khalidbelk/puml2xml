13bb984adf06a4e79604ca682485418535661609
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2025-02-17 13:40:35 +0100

    feat: add puml2xml versioning to XMI header

--- src/xml_chunks.ml
+++ src/xml_chunks.ml
@@ -12,6 +12,7 @@ let xml_header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><XMI xmlns:UML=\"hre
             <XMI.exporter>PlantUML</XMI.exporter>
             <XMI.exporterVersion>1.2024.7</XMI.exporterVersion>
             <XMI.creator>puml2xml</XMI.creator>
+            <XMI.creatorVersion>1.2.0</XMI.creatorVersion>
         </XMI.documentation>
         <XMI.metamodel xmi.name=\"UML\" xmi.version=\"1.4\"/>
     </XMI.header>
